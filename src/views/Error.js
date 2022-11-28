52e35ed9ce2c02f8d0cafbc22d0c80b6081fc859
Ederjoel26 <eder_1321124031@uptecamac.edu.mx>
2022-11-28 16:16:27 -0600

    Added page Error 404

--- src/views/Error.js
+++ src/views/Error.js
@@ -1,10 +1,21 @@
-export const Error = () => {
+import '../styles/Error.css';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
+export const Error = () => {
     return(
-        <div>
-            <center>
-                <h1>Error 404 u.u</h1>
-            </center>
+        <div className="bg-light">
+             <div className="container">
+                <div className="card text-center position-absolute top-50 start-50 translate-middle shadow-lg">
+                    <div className="card-body">
+                    <h1 className="h1 font-roboto txt-header card-title">404</h1>
+                    <p className="h3 font-roboto p-2 card-text">Oops.. Esta página no ha sido encontrada.</p>
+                            <p className="h6">Esta página no parece existir.
+                            <br/> No te sientas mal, permitanos ayudarlo a volver a su camino.</p>
+                        <a type="button" className="btn btn-primary col-6 font-roboto mt-4 mb-5" href="/feed">Regresar a la página principal</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
