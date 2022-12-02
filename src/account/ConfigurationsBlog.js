5700c42afe3806d94ba1ca2202999e3b85eaf718
Ederjoel26 <Ederjoel26>
2022-12-02 02:37:04 -0600

    Rectify changes

--- src/account/ConfigurationsBlog.js
+++ src/account/ConfigurationsBlog.js
@@ -92,9 +92,9 @@ export const ConfigurationsBlog = () => {
                 <form onSubmit={ insertArchive }>
                     <h1> Configuraciones del blog </h1>
                     <h5>Foto de perfil: </h5> <br/>
-                    <input type='file' accept='image/png, image/jpeg' onChange={(e) => uploadImgProfile(e.target.files[0])} /> <br/> 
+                    <input type='file' required={true} accept='image/png, image/jpeg' onChange={(e) => uploadImgProfile(e.target.files[0])} /> <br/> 
                     <h5>Foto de portada: </h5> <br/>
-                    <input type='file' accept='image/png, image/jpeg' onChange={(e) => uploadImgBackground(e.target.files[0])}/> <br/>
+                    <input type='file' required={true} accept='image/png, image/jpeg' onChange={(e) => uploadImgBackground(e.target.files[0])}/> <br/>
                     <input type='submit'/>
                 </form>   
             </center>
--- src/account/SelectCategories.js
+++ src/account/SelectCategories.js
@@ -1,11 +1,34 @@
-
+import axios from 'axios';
+import { useEffect, useMemo, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 export const SelectCategories = () => {
+    
+    const navigate = useNavigate();
+    const [categories, setCategories] = useState([]);
 
+    const gettingCategories = async () => {
+        const category = await axios({
+            url: 'https://apireadamblog-production.up.railway.app/category/all',
+            method: 'get'
+        });
+        category.data.map(d => {
+            if(!categories.includes(d.categoryName)){
+                categories.push(d.categoryName)
+            }
+        });
+    }
+
+    useMemo(() => {
+        gettingCategories();
+        console.log(categories);
+    });
 
     return(
         <div>
-            Hola
+            {
+                categories.map(d => <input type = 'button' key={d} value={d} />)
+            }
         </div>
     );
 }
